7f846591ec5cbe6dfc38590041e8353443f11de4
ansh <ansh.mulchandani01@estudiant.upf.edu>
2026-05-27 19:22:51 +0200

    requieres both players to start the game

--- Assets/Scripts/Title/Startbutton.cs
+++ Assets/Scripts/Title/Startbutton.cs
@@ -1,15 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    private List<GameObject> playersOnButton = new List<GameObject>();
+
+    public int requiredPlayersToStart = 2;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("SampleScene"); 
+            if (!playersOnButton.Contains(other.gameObject))
+            {
+                playersOnButton.Add(other.gameObject);
+            }
+
+            // Check if both players are now inside the trigger
+            if (playersOnButton.Count >= requiredPlayersToStart)
+            {
+                SceneManager.LoadScene("SampleScene"); 
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // If a player steps off the button, remove them from the list        if (other.CompareTag("Player"))
+        {
+            if (playersOnButton.Contains(other.gameObject))
+            {
+                playersOnButton.Remove(other.gameObject);
+            }
         }
     }
 }
